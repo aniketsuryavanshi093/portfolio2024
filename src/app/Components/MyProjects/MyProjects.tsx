47d74206e467bf42e476821f3868e69f94fbb06e
= <=>
2023-12-21 20:57:01 +0530

    side bar added conditionally git streak added

--- src/app/Components/MyProjects/MyProjects.tsx
+++ src/app/Components/MyProjects/MyProjects.tsx
@@ -1,44 +1,115 @@
-'use client'
-import SectionWrapper from '@/app/hoc/StaggerContainer'
-import React from 'react'
-import { motion } from "framer-motion"
-import { zoomIn } from '@/app/utils'
-import "./mymproject.scss"
-import Image from 'next/image'
-import Link from 'next/link'
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import SectionWrapper from "@/app/hoc/StaggerContainer";
+import React from "react";
+import { motion } from "framer-motion";
+import { zoomIn } from "@/app/utils";
+import "./mymproject.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 const MyProjects = () => {
-    return (
-        <div className="w-full">
-            <motion.p variants={zoomIn(0.6, .75)} className='text-2xl  text-center w-full'>Projects</motion.p>
-            <div className='flex lg:flex-row flex-col xl:justify-start justify-between  gap-4 mt-9 align-center'>
-                <Link href={"project/tasksphere"} prefetch={false} className='projectcard lg:rounded-[30px] rounded-[10px] p-1 flex-1'>
-                    <Image src="/images/project1.png" className='lg:rounded-[30px] rounded-[10px]' unoptimized width={100} height={250} alt='project1' />
-                    <div className='lg:w-[80%] w-full m-auto flex justify-start my-3 gap-4 align-center'>
-                        <Image alt='scale' unoptimized width={30} height={30} className='w-[30px] scaleimage' src={"/images/scale.png"} />
-                        <div className='flex flex-col align-start justify-center'>
-                            <p className=' 
-                            lg:text-lg text-sm'> TaskSphere (twilio) </p>
-                            <p className=' 
-                          lg:text-base  xl:text-lg text-sm text-mainpara'>My Design Projects</p>
-                        </div>
-                    </div>
-                </Link>
-                <Link href={"project/upwork"} prefetch={false} className='projectcard lg:rounded-[30px] rounded-[10px] p-1 flex-1'>
-                    <Image src="/images/project2.png" className='lg:rounded-[30px] rounded-[10px]' unoptimized width={100} height={250} alt='project1' />
-                    <div className='lg:w-[80%] w-full m-auto flex justify-start my-3 gap-4 align-center'>
-                        <Image alt='scale' unoptimized width={30} height={30} className='w-[30px] scaleimage' src={"/images/scale.png"} />
-                        <div className='flex flex-col align-start justify-center'>
-                            <p className=' 
-                           lg:text-lg text-sm'>  FreelanceWeb (Upwork)</p>
-                            <p className=' 
-                             lg:text-base  xl:text-lg text-sm text-mainpara'>My Design Projects</p>
-                        </div>
-                    </div>
-                </Link>
+  return (
+    <div className="w-full">
+      <motion.p
+        variants={zoomIn(0.6, 0.75)}
+        className="text-2xl  text-center w-full"
+      >
+        Projects
+      </motion.p>
+      <div className="flex lg:flex-row flex-col xl:justify-start justify-between  gap-4 mt-9 align-center">
+        <Link
+          href={"project/tasksphere"}
+          prefetch={false}
+          className="projectcard lg:rounded-[30px] rounded-[10px] p-1 flex-1"
+        >
+          <Image
+            src="/images/project1.png"
+            className="lg:rounded-[30px] rounded-[10px]"
+            unoptimized
+            width={100}
+            height={250}
+            alt="project1"
+          />
+          <div className="lg:w-[80%] w-full m-auto flex justify-start my-3 gap-4 align-center">
+            <Image
+              alt="scale"
+              unoptimized
+              width={30}
+              height={30}
+              className="w-[30px] scaleimage"
+              src={"/images/scale.png"}
+            />
+            <div className="flex flex-col align-start justify-center">
+              <p
+                className=" 
+                            lg:text-lg text-sm"
+              >
+                TaskSphere (twilio)
+              </p>
+              <p className="lg:text-base  xl:text-lg text-sm text-mainpara">
+                My Design Projects
+              </p>
             </div>
+          </div>
+        </Link>
+        <Link
+          href={"project/upwork"}
+          prefetch={false}
+          className="projectcard lg:rounded-[30px] rounded-[10px] p-1 flex-1"
+        >
+          <Image
+            src="/images/project2.png"
+            className="lg:rounded-[30px] rounded-[10px]"
+            unoptimized
+            width={100}
+            height={250}
+            alt="project1"
+          />
+          <div className="lg:w-[80%] w-full m-auto flex justify-start my-3 gap-4 align-center">
+            <Image
+              alt="scale"
+              unoptimized
+              width={30}
+              height={30}
+              className="w-[30px] scaleimage"
+              src={"/images/scale.png"}
+            />
+            <div className="flex flex-col align-start justify-center">
+              <p
+                className=" 
+                           lg:text-lg text-sm"
+              >
+                FreelanceWeb (Upwork)
+              </p>
+              <p
+                className=" 
+                             lg:text-base  xl:text-lg text-sm text-mainpara"
+              >
+                My Design Projects
+              </p>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className="flex mt-14 lg:flex-row flex-col gap-4 flex-row justify-between items-center">
+        <div className="w-10/12 lg:w-1/2 flex flex-col items-center" >
+                <p className="text-lg">My Streak</p>
+                <img className="w-full h-32 lg:h-52 mt-3" src="https://github-readme-streak-stats.herokuapp.com/?user=aniketsuryavanshi093&theme=dark" alt="everly-gif" width="70%"></img>
         </div>
-    )
-}
+        <div className="w-10/12 lg:w-1/2  flex flex-col items-center">
+            <p className="text-lg">
+                My Git stats
+            </p>
+            <img className="w-full h-32 lg:h-52 mt-3" src="https://github-readme-stats.vercel.app/api?username=aniketsuryavanshi093&show_icons=true&locale=en&theme=dark" alt="everly-gif" ></img>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SectionWrapper(MyProjects, "projects", 'flex justify-between lg:w-[90%] w-full py-5 flex-col md:mt-[100px] px-4')
+export default SectionWrapper(
+  MyProjects,
+  "projects",
+  "flex justify-between lg:w-[90%] w-full py-5 flex-col md:mt-[100px] px-4"
+);
--- src/app/Components/Sidebar/SideBar.tsx
+++ src/app/Components/Sidebar/SideBar.tsx
@@ -1,61 +1,69 @@
-'use client'
-import React, { useCallback, useEffect, useState } from 'react'
-import "./sidebar.scss"
+"use client";
+import React, { useState } from "react";
+import "./sidebar.scss";
 import { Tooltip } from "@nextui-org/react";
-import Link from 'next/link';
-
+import Link from "next/link";
+import {  usePathname,  } from "next/navigation";
 
 type sidebaroptions = {
-    toltipcontent: string,
-    icon: string,
-    redirecthref: string,
-}
+  toltipcontent: string;
+  icon: string;
+  redirecthref: string;
+};
 
 const SideBar = () => {
-    const [SelectedIcon, setSelectedIcon] = useState("myself")
-    const sidebarobject: sidebaroptions[] = [
-        {
-            toltipcontent: "myself",
-            icon: "fa-solid fa-house",
-            redirecthref: "#myself",
-        }
-        , {
-            toltipcontent: "About Me",
-            icon: "fa-solid fa-user",
-            redirecthref: "#about",
-        }
-        , {
-            toltipcontent: "What i Offer",
-            icon: "fa-solid fa-book-open-reader",
-            redirecthref: "",
-        }, {
-            toltipcontent: "My Projects",
-            icon: "fa-solid fa-diagram-project",
-            redirecthref: "/projects",
-        }, {
-            toltipcontent: "Contact me",
-            icon: "fa-solid fa-address-book",
-            redirecthref: "#contact",
-        }
-    ]
-    return (
-        <div className='fixed py-1 z-[999999] flex flex-row lg:flex-col  justify-center align-center gap-2 w-auto lg:w-16 h-auto lg:top-12 top-[1%] left-[12%]   lg:left-11 sidebar'>
-            {
-                sidebarobject?.map((elem: sidebaroptions) => (
-                    <Tooltip key={elem.icon} content={elem.toltipcontent} placement="right">
-                        <Link href={elem.redirecthref}>
-                            <div onClick={() => setSelectedIcon(elem.toltipcontent)} className={`${SelectedIcon === elem.toltipcontent && "selectedicon"} cursor-pointer sidebaricons h-12 w-12  flex flex-col justify-center align-center`}>
-                                <i className={`${elem.icon} `}></i>
-                                {SelectedIcon === elem.toltipcontent && (
-                                    <div className='mb-0  selecteddot mt-1'></div>
-                                )}
-                            </div>
-                        </Link>
-                    </Tooltip>
-                ))
-            }
-        </div>
-    )
-}
+  const [SelectedIcon, setSelectedIcon] = useState("myself");
+  const sidebarobject: sidebaroptions[] = [
+    {
+      toltipcontent: "myself",
+      icon: "fa-solid fa-house",
+      redirecthref: "#myself",
+    },
+    {
+      toltipcontent: "About Me",
+      icon: "fa-solid fa-user",
+      redirecthref: "#about",
+    },
+    {
+      toltipcontent: "What i Offer",
+      icon: "fa-solid fa-book-open-reader",
+      redirecthref: "",
+    },
+    {
+      toltipcontent: "My Projects",
+      icon: "fa-solid fa-diagram-project",
+      redirecthref: "/projects",
+    },
+    {
+      toltipcontent: "Contact me",
+      icon: "fa-solid fa-address-book",
+      redirecthref: "#contact",
+    },
+  ];
+  const params = usePathname();
+  console.log(params);
 
-export default SideBar
+  return params === "/" ? (
+    <div className="fixed py-1 z-[999999] flex flex-row lg:flex-col  justify-center align-center gap-2 w-auto lg:w-16 h-auto lg:top-12 top-[1%] left-[12%]   lg:left-11 sidebar">
+      {sidebarobject?.map((elem: sidebaroptions) => (
+        <Tooltip key={elem.icon} content={elem.toltipcontent} placement="right">
+          <Link href={elem.redirecthref}>
+            <div
+              onClick={() => setSelectedIcon(elem.toltipcontent)}
+              className={`${
+                SelectedIcon === elem.toltipcontent && "selectedicon"
+              } cursor-pointer sidebaricons h-12 w-12  flex flex-col justify-center align-center`}
+            >
+              <i className={`${elem.icon} `}></i>
+              {SelectedIcon === elem.toltipcontent && (
+                <div className="mb-0  selecteddot mt-1"></div>
+              )}
+            </div>
+          </Link>
+        </Tooltip>
+      ))}
+    </div>
+  ) : null
+};
+
+export default SideBar;
